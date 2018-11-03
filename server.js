1ede10959599875f391d2ccdb44d674ba681f3b8
Glitch (disboard-tet) <none>
2018-11-03 02:20:50 +0000

    🌋🎱 Updated with Glitch

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/", (request, response) => {
 app.listen(process.env.PORT);
 
 setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`); 
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 // http://disboard-tet.glitch.me
 
@@ -24,6 +24,7 @@ setInterval(() => {
 const Discord = require('discord.js');
 //const richEmbed = require('discord.js');
 const client = new Discord.Client();
+const guild = new Discord.Guild();
 const config = require("./config.json");
 
 let prefix = process.env.PREFIX;
@@ -42,80 +43,75 @@ client.on('ready', () => {
 });
 
 client.on('message', async message => {
-  
-  if(message.author.bot) return;
-  
-  if(message.content.indexOf(prefix) !== 0) return;
-  
+
+  if (message.author.bot) return;
+
+  if (message.content.indexOf(prefix) !== 0) return;
+
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  if (command === "ping") {
-   let ping = Math.floor(message.client.ping);
-   // let ping = Math.round(message.client.ping);
-   message.channel.send(':ping_pong: Latencia: `'+ping+' ms.`'); 
-  }
-  
-  if (command === "tag") {
+  if (command === "ping" && args.length <= 0) {
+    let ping = Math.floor(message.client.ping);
+    message.channel.send(':ping_pong: Latencia: `' + ping + ' ms.`');
+  } else if (command === "tag" && args.length <= 0) {
     message.channel.send({
       embed: {
         color: config.color,
-        description: "- Tag:**『 』**\n- Tag secundario: _(espacio en blanco)_\n- Tag terciario: **DB** (solo en caso de no poder escribir el primario)\n- Nombre: **Disboard**"    
-      } 
+        description: "- Tag: **『 』**\n- Tag secundario: _(espacio en blanco)_\n- Tag terciario: **DB** (solo en caso de no poder escribir el primario)\n- Nombre: **Disboard**"
+      }
     });
-  }
-  
-  if (command === "tabla") {
+  } else if (command === "tabla" && args.length <= 0) {
     message.channel.send({
       embed: {
         color: config.color,
-        description: "Tag: **『 』**\n\nGenerador: [https://hlorenzi.github.io/mk8d_ocr/table.html](https://hlorenzi.github.io/mk8d_ocr/table.html)"    
-      } 
+        description: "- Tag: **『 』**\n\n- Generador: [https://hlorenzi.github.io/mk8d_ocr/table.html](https://hlorenzi.github.io/mk8d_ocr/table.html)\n\n_Ejemplo:_",
+        "image": {
+          "url": "https://cdn.discordapp.com/attachments/487343815615578124/503632252807413762/Screen_Shot_2018-10-21_at_1.14.33_PM.png"
+        }
+      }
     });
-  }
-  
-  if (command === "disboard") {
-    let imanity = message.guild.members.filter(member => { 
-        return member.roles.find("name", "Imanity");
+  } else if (command === "disboard" && args.length <= 0) {
+    let imanity = message.guild.members.filter(member => {
+      return member.roles.find("name", "Imanity");
     }).map(member => {
-        return member.user.username;
+      return member.user.username;
     })
-    let seirens = message.guild.members.filter(member => { 
-        return member.roles.find("name", "Seirens");
+    let seirens = message.guild.members.filter(member => {
+      return member.roles.find("name", "Seirens");
     }).map(member => {
-        return member.user.username;
+      return member.user.username;
     })
-    let werebeast = message.guild.members.filter(member => { 
-        return member.roles.find("name", "Werebeast");
+    let werebeast = message.guild.members.filter(member => {
+      return member.roles.find("name", "Werebeast");
     }).map(member => {
-        return member.user.username;
+      return member.user.username;
     })
     message.channel.send({
       embed: {
         "color": config.color,
-        "title": "**Listado de miembros**",
-        "thumbnail": {
-          "url": message.guild.iconURL
-        },
-        "fields": [          
+        "fields": [{
+            "name": "**Listado de miembros**",
+            "value": ":busts_in_silhouette: - " + (imanity.length + seirens.length + werebeast.length),
+          },
           {
-            "name": "Miembros",
+            "name": "Miembros - " + imanity.length,
             "value": imanity.join("\n"),
             "inline": true
           },
           {
-            "name": "Allys",
-            "value": werebeast.join("\n"),
-            "inline": true
-          },
-          {
-            "name": "Trials",
+            "name": "Trials - " + seirens.length,
             "value": seirens.join("\n"),
             "inline": true
           },
           {
-            "name": "**Enlaces**",
-            "value": "-",
+            "name": "Allys - " + werebeast.length,
+            "value": werebeast.join("\n"),
+            "inline": true
+          },
+          {
+            "name": "\n**Enlaces**",
+            "value": ":link:",
           },
           {
             "name": "Web",
@@ -128,21 +124,18 @@ client.on('message', async message => {
             "inline": true
           }
         ]
-      } 
+      }
     });
-  }
-  
-  if (command === "wars") {  
+  } else if (command === "wars" && args.length <= 0) {
     let date = new Date();
     message.channel.send({
       embed: {
         "color": config.color,
-        "title": "Wars disputadas: **_pendiente_**",
+        "title": "Wars disputadas: pendiente",
         "thumbnail": {
           "url": message.guild.iconURL
         },
-        "fields": [
-          {
+        "fields": [{
             "name": "Victorias",
             "value": "_pendiente_",
             "inline": true
@@ -163,108 +156,200 @@ client.on('message', async message => {
             "inline": true
           }
         ]
-      } 
+      }
     });
-  }
-  
-  if (command === "info") {  
+  } else if (command === "info" && args.length <= 0) {
     let joinedTimestamp = message.guild.joinedTimestamp;
     let date = new Date(joinedTimestamp);
-    
+
     message.channel.send({
       embed: {
         color: config.color,
-        description: "Miembro desde: " + date.getDay() + "/" + date.getMonth() + "/" + date.getFullYear().toString().substr(2,2) + "\n\n_Próximamente: estadísticas del jugador_",
+        description: "Miembro desde: _pendiente_", // + date.getDay() + "/" + date.getMonth() + "/" + date.getFullYear().toString().substr(2, 2) + "\n\n_Próximamente: estadísticas del jugador_",
         "thumbnail": {
           "url": message.author.avatarURL
         },
         "author": {
           "name": message.author.username
         },
-        "fields": [
-          {
+        "fields": [{
             "name": "Wars jugadas",
             "value": "_pendiente_",
             "inline": true
           },
           {
-            "name": "Media",
+            "name": "Media pts.",
             "value": "_pendiente_",
             "inline": true
           },
           {
-            "name": "Victorias",
+            "name": "Ratio V/D",
             "value": "_pendiente_",
             "inline": true
-          },
-          {
-            "name": "Derrotas",
-            "value": "_pendiente_",
-            "inline": true
-          }          
+          }
         ]
       }
-      
     });
-  } 
-  
-  if (command === "r") {
+  } else if (command === "info" && args.length == 1) {
+    if (!(args[0])) {
+      message.channel.send({
+      embed: {
+        color: config.color,
+        description: "**Tet no reconoce este formato**.\nTal vez quisistes usar `+info @[nombre-usuario]`."
+      }
+    });
+    } else {
+      let joinedTimestamp = message.guild.joinedTimestamp;
+      let date = new Date(joinedTimestamp);
+
+      message.channel.send({
+        embed: {
+          color: config.color,
+          description: "Miembro desde: " + date.getDay() + "/" + date.getMonth() + "/" + date.getFullYear().toString().substr(2, 2) + "\n\n_Próximamente: estadísticas del jugador_",
+          "thumbnail": {
+            "url": message.author.avatarURL
+          },
+          "author": {
+            "name": message.author.username
+          },
+          "fields": [{
+              "name": "Wars jugadas",
+              "value": "_pendiente_",
+              "inline": true
+            },
+            {
+              "name": "Media pts.",
+              "value": "_pendiente_",
+              "inline": true
+            },
+            {
+              "name": "Ratio V/D",
+              "value": "_pendiente_",
+              "inline": true
+            }
+          ]
+        }
+      });
+    }
+  } else if (command === "r" && args.length <= 0) {
     let gifs = [
       "https://i.pinimg.com/originals/86/bb/66/86bb66cb4f524bef1f7bc1e45733cfe7.gif",
       "https://media.giphy.com/media/MJDBXJ2PtgkEg/giphy.gif",
       "https://media.giphy.com/media/CO9pHUyGzEoH6/giphy.gif",
-      "https://media.giphy.com/media/FAEj6nbeR1SCI/giphy.gif",
-      "https://media.giphy.com/media/uEV4RB1fjv3AA/giphy.gif",
       "https://media.giphy.com/media/FGbEffWs9la1O/giphy.gif",
       "https://media.giphy.com/media/QcT4JVFDn4WEU/giphy.gif",
       "https://media.giphy.com/media/kXFpgStKE2Ypi/giphy.gif",
-      "https://media.giphy.com/media/YdXoD3Kk0pxw4/giphy.gif",
       "https://media0.giphy.com/media/CphBpuaoIjwk0/giphy.gif",
       "https://media.giphy.com/media/fGepS5ZgnsMNi/giphy.gif",
       "https://media.giphy.com/media/MOTwD54jp6d9u/giphy.gif",
       "https://media.giphy.com/media/nf0TvJDBOTfjy/giphy.gif",
       "https://media.giphy.com/media/gHMpcRSayJu8w/giphy.gif",
-      "https://media.giphy.com/media/8i24nfqlvkmB2/giphy.gif",
       "https://media.giphy.com/media/SaiULvpd3yQow/giphy.gif",
       "https://media.giphy.com/media/fQF0FrBcSHKsU/giphy.gif",
       "https://media.giphy.com/media/2SRXHjVfsXAYw/giphy.gif"
     ];
-    
+
     let videos = [
       "https://media.giphy.com/media/Iv9a7T9pn38sw/giphy-hd.mp4"
     ]
-    let rand = Math.floor(Math.random() * gifs.length);    
+    let rand = Math.floor(Math.random() * gifs.length);
     message.channel.send({
       embed: {
         color: config.color,
         "image": {
           "url": gifs[rand]
-        }  
-      } 
+        }
+      }
     });
-  } 
-  
-  // Si +aschente werebeast o si +aschente immanity, ver k roles tiene, segun uno u otro, mostrar un mensaje u otro
-  
-  if (command === "help") {    
+  } else if (command === "aschente" && args.length == 1) {
+    
+    let web_extra = "Al formar parte de Disboard, usamos todos tus resultados de wars para hacer estadísticas que puedes consultar a través de este bot `+help` o a través de la web https://disboard.team _[próximamente]_";
+    
+    let usuario = message.mentions.members.first();
+    
+    if (usuario) {
+    
+      if(usuario.roles.find("name", "Imanity")) {
+         message.channel.send({
+          embed: {
+            color: config.color,
+            description: "¡Bienvenido al clan de **Disboard** " + usuario + "!\n\nPor favor, tómate tu tiempo para leerte los <#487350168853676053> (normas) e <#488091660115247105> para entender un poco mejor el servidor.\n\nNo olvides presentarte en <#487380356874633217> para que te conozcamos todos un poco mejor :)"
+          }
+        });
+        message.channel.send({
+          embed: {
+            color: 14161240,
+            description: web_extra
+          }
+        });
+       } else if(usuario.roles.find("name", "Seirens")) {
+         message.channel.send({
+          embed: {
+            color: config.color,
+            description: "¡Bienvenido al clan de **Disboard** " + usuario + "!\n\nPor favor, tómate tu tiempo para leerte los <#487350168853676053> (normas) e <#488091660115247105> para entender un poco mejor el servidor.\n\nNo olvides presentarte en <#487380356874633217> para que te conozcamos todos un poco mejor :)\n\nTras jugar algunas wars junto al equipo, los representantes pasarán a ponerte rol de miembro de clan."
+          }
+        });
+        message.channel.send({
+          embed: {
+            color: 14161240,
+            description: web_extra
+          }
+        });
+       } else if(usuario.roles.find("name", "Werebeast")) {
+         message.channel.send({
+          embed: {
+            color: config.color,
+            description: "¡Bienvenido al clan de **Disboard** " + usuario + "!\n\nPor favor, tómate tu tiempo para leerte los <#487350168853676053> (normas) e <#488091660115247105> para entender un poco mejor el servidor.\n\nPásatelo bien y sobretodo, respeta las reglas y la forma de ser de este clan :)"
+          }
+        });
+        message.channel.send({
+          embed: {
+            color: 14161240,
+            description: web_extra
+          }
+        });
+       } else {
+         message.channel.send({
+          embed: {
+            color: config.color,
+            description: "¡Bienvenido al clan de **Disboard** " + usuario + "!\n\nPor favor, tómate tu tiempo para leerte los <#487350168853676053> (normas) e <#488091660115247105> para entender un poco mejor el servidor.\n\nNo olvides presentarte en <#487380356874633217> para que te conozcamos todos un poco mejor :)"
+          }
+        });
+        message.channel.send({
+          embed: {
+            color: 14161240,
+            description: web_extra
+          }
+        });
+       }   
+    }
+    
+  } else if (command === "help" && args.length <= 0) {
     message.channel.send({
       embed: {
         color: config.color,
-        description: "Bot de ayuda general\n\n**+tag** -> Referencias al clan\n**+tabla** -> Información para hacer correctamente las tablas\n**+info** -> Información del jugador\n**+wars** -> Informe de wars\n**+disboard** -> Datos del clan\n**+r** -> Gifs random de NGNL\n**+help** -> Lista de comandos disponibles"    
-      } 
+        description: "Bot de ayuda general\n\n**+tag** -> Referencias al clan\n**+tabla** -> Información para hacer correctamente las tablas\n**+info** -> Información del jugador\n**+wars** -> Informe de wars\n**+disboard** -> Datos del clan\n**+invi** -> Enlace de invitación\n**+r** -> Gifs random de NGNL\n**+help** -> Lista de comandos disponibles"
+      }
     });
-  } 
-  
-  
-  if (!(command === "ping" || command === "tag" || command === "help" || command === "r" || command === "tabla" || command === "info" || command === "disboard" || command === "wars")) {   
+  } else if (command === "invi" && args.length <= 0) {
     message.channel.send({
       embed: {
         color: config.color,
-        description: "**Tet no reconoce tu comando**.\nTet sigue aprendiendo, pronto tendrá más comandos que ofrecer."    
-      } 
+        description: "Invitación al servidor:\n\nhttps://discord.gg/YmFtq7m"
+      }
     });
+  } else {
+    /*message.channel.send({
+      embed: {
+        color: config.color,
+        description: "**Tet no reconoce tu comando**."
+      }
+    });*/
   }
-  
+
 });
+
+
+// Si +aschente werebeast o si +aschente immanity, ver k roles tiene, segun uno u otro, mostrar un mensaje u otro
 
 client.login(process.env.TOKEN);
